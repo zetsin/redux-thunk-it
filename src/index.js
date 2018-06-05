d13a85ba072969c48a8cf81c05fd8f1c52d2f77a
Zetsin <zetsin@gmail.com>
2018-06-05 23:47:07 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import thunk from 'redux-thunk'
+
+export function combineReducers(models) {
+  const reducers = {}
+  Object.keys(models).forEach(name => {
+    const model = models[name]
+    model.state = model.state || {}
+    model.reducers = model.reducers || {}
+
+    reducers[name] = function(state = model.state, action) {
+      const [target, type] = action.type.split('/')
+      const reducer = model.reducers[type]
+      if(target === name) {
+        if(reducer) {
+          return reducer(state, action.payload)
+        }
+        else {
+          throw new Error('Reducer Not Found')
+        }
+      }
+      else if(target === '') {
+        if(reducer) {
+          return reducer(state, action.payload)
+        }
+      }
+      return state
+    }
+  })
+  return reducers
+}
+
+export function thunkActions(model) {
+  const actions = {}
+
+  Object.keys(model.actions || {}).forEach(name => {
+    actions[name] = function(...args) {
+      return (dispatch, getState, extraArgument={}) => {
+        Object.assign(this, extraArgument, { dispatch, getState })
+        return model.actions[name].call(this, ...args)
+      }
+    }
+  })
+
+  return actions
+}
+
+export default thunk
